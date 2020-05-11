5428b8859bd5eef668abaeb874d87adfee600384
Viral Haria <viralhar@gmail.com>
2020-05-11 10:37:36 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+if(document.readyState === 'loading'){
+    document.addEventListener('DOMContentLoaded', domLoaded);
+}else{
+    domLoaded();
+}
+
+function domLoaded(){
+    console.log("dom ready");
+    
+    const keys = Array.from(document.querySelectorAll(`.key`));
+    console.log(keys);
+    keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+    window.addEventListener('keydown', playSound);
+}
+
+function playSound(e){  
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if(!audio) return;
+
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
+}
+
+function removeTransition(e){
+    //console.log(e);
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
